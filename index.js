848dfcc1ab4f415670c23efb27a95623f149ca2d
anisetiani <anisetiani178Gmail.com>
2021-09-18 16:33:54 +0700

    first commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const text2 = document.querySelector("#name");
 const text3 = document.querySelector("#login");
 
 
+
 const getDataAsync = async () => {
     let response = await fetch("https://api.github.com/users/anisetiani");
     let result = await response.json();
@@ -13,22 +14,24 @@ const getDataAsync = async () => {
     const name = document.getElementById("name");
     const login = document.getElementById("login");
     const bio = document.getElementById("bio");
-
+    
     console.log(result);
     name.innerHTML = result.name;
     login.innerHTML = result.login;
     bio.innerHTML = result.bio;
+    
+
 
     document.getElementById("myBtn").addEventListener("click", function() {
         alert("Hallo Salam kenal yaa!! :)");
       });
 
-    document.body.style.backgroundColor = "white";
-    document.getElementById("nama").style.color = "#5B8A72";
+    document.body.style.backgroundColor = "#A39999";
+    document.getElementById("nama").style.color = "#443B3B";
     document.getElementById("nama").style.fontFamily = "monospace";
-    document.getElementById("login").style.color = "#3B5249";
-    document.getElementById("bio").style.color = "#3B5249";
-    document.getElementById("name").style.color = "#3B5249";
+    document.getElementById("login").style.color = "#443B3B";
+    document.getElementById("bio").style.color = "#443B3B";
+    document.getElementById("name").style.color = "#443B3B";
     document.getElementById("name").style.fontFamily = "monospace";
     document.getElementById("bio").style.fontFamily = "monospace";
     document.getElementById("login").style.fontFamily = "monospace";
